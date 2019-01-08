2db5bb6c87c06049e31a40708fe399f207d19cec
klehr <kyolehr@gmail.com>
2019-01-08 15:01:20 -0500

    implement python unittest framework

--- redfin_script.py
+++ redfin_script.py
@@ -1,24 +1,36 @@
 import unittest
 from redfin_po import Redfin
 
-class RedfinTest(unittest.TestCase):
+
+class TestRedfin(unittest.TestCase):
     def setUp(self):
         self.redfin = Redfin()
 
-    def filter_result(self):
+    def test_filter(self):
         self.redfin.go_to_page()
         self.redfin.main_search('Sunnyside, NY')
         self.redfin.select_area()
+
+        homecount_in_mainview = self.redfin.get_homecount_mainview()
+        # Home count must be > 0.
+        if homecount_in_mainview == '0 Homes':
+            raise Exception('Site returned {}'.format(homecount_in_mainview))
+
         self.redfin.show_filters()
         self.redfin.set_property_type_condo()
         self.redfin.set_property_type_other()
         self.redfin.toggle_for_sale_toggle()
+        homecount_in_filterview_filter_applied = self.redfin.get_homecount_filterview()
 
-        prefilter_result = redfin.prefilter_result()
-        redfin.apply_filters()
-        postfilter_result = redfin.postfilter_result()
+        # Home count should not be equal to pre-filter value.
+        assert homecount_in_mainview != homecount_in_filterview_filter_applied
+        self.redfin.apply_filters()
+        homecount_in_mainview_filter_applied = self.redfin.get_homecount_mainview()
 
-        assert prefilter_result in postfilter_result, 'Prefilter result{pre_filter} does not match postfilter result{post_filter}'.format(pre_filter=prefilter_result, post_filter=postfilter_result)
+        # Home count in filter view and main view should be the max number of home count in main view.
+        assert homecount_in_filterview_filter_applied in homecount_in_mainview_filter_applied, \
+            'Prefilter result{prefilter_count} does not match postfilter result{mainview_count}'.format(prefilter_count=homecount_in_filterview_filter_applied, mainview_count=homecount_in_mainview_filter_applied)
+
 
 if __name__ == '__main__':
     unittest.main()
